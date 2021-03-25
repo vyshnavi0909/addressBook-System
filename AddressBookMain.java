8358fcc21ba0675c414613dfebc9aea9d51cbd45
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-25 12:22:00 +0530

    ADD: new contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,6 +3,14 @@ public class AddressBookMain {
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Address Book Program");
+
+		Contacts x = new Contacts("Vyshu", "Goud", "Go Green Heights", "Hyd", "Telangana", 500010, 9700000099L, "vyshugoud11@gmail.com");
+
+      System.out.println("Name: " + x.firstName + " " + x.lastName );
+      System.out.println("Address: " + x.address + ", " + x.cityName + ", " + x.state + ", " + x.zip );
+      System.out.println("Contact number: " + x.phoneNum);
+      System.out.println("Email id: " + x.email);
+
 	}
 }
 
