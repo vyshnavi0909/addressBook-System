f87082119689300ac68d97154c104e3f4a350cd7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-25 12:17:58 +0530

    ADD: created a class for contact info

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,26 @@
 public class AddressBookMain {
+
+
 	public static void main(String[] args){
 		System.out.println("Welcome to Address Book Program");
+	}
+}
+
+class Contacts{
+
+	String firstName, lastName, address, cityName, state, email;
+	int zip;
+	long phoneNum;
+
+	Contacts(String fN, String lN, String addr, String city, String st, int z, long pNum, String eId){
+		firstName = fN;
+		lastName = lN;
+		address = addr;
+		cityName = city;
+		state = st;
+		zip = z;
+		phoneNum = pNum;
+		email = eId;
+
 	}
 }
