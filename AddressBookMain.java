cdf008706ae7f642314134e347e99832e8287dcb
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-03-30 10:19:52 +0530

    Modified: method names

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,7 +8,7 @@ public class AddressBookMain {
     static String firstPerson, secondPerson;
 
     //reading input for string variables for contact info
-    static String UserInput1(String a){
+    static String userInput1(String a){
         Scanner scan = new Scanner(System.in);
         System.out.println(a);
         String x = scan.nextLine();
@@ -16,7 +16,7 @@ public class AddressBookMain {
     }
 
     //reading input for long variables for contact info
-    static long UserInput2(String a){
+    static long userInput2(String a){
         Scanner scan = new Scanner(System.in);
         System.out.println(a);
         long y = scan.nextLong();
@@ -25,64 +25,64 @@ public class AddressBookMain {
 
     //assigning variables to pass into the contacts
     static void inputCall(){
-        fn = UserInput1("First Name: ");
-        ln = UserInput1("Last Name: ");
-        cn = UserInput1("City: ");
-        sn = UserInput1("State: ");
-        pin = UserInput2("Zip: ");
-        phN = UserInput2("phoneNumber: ");
-        eid = UserInput1("Email id: ");
+        fn = userInput1("First Name: ");
+        ln = userInput1("Last Name: ");
+        cn = userInput1("City: ");
+        sn = userInput1("State: ");
+        pin = userInput2("Zip: ");
+        phN = userInput2("phoneNumber: ");
+        eid = userInput1("Email id: ");
     }
 
     //providing options for user to select "what to do next?"
-    static void Options(){
+    static void options(){
         Scanner scan = new Scanner(System.in);
         System.out.println("What action do you want to perform next? \n1).Add another person details \n2).Edit the existing person details. \n3).Delete the person details. \n4).For exiting.");
         d = scan.nextInt();
     }
 
-    static void ToChangeDetails(Contacts obj){
-        Scanner uc = new Scanner(System.in);
+    static void toChangeDetails(Contacts obj){
+        Scanner input = new Scanner(System.in);
         System.out.println("Which one do you want to change? \n1).First Name \n2).Last Name \n3).City \n4).State \n5).Zip \n6).Phone Number \n7).Email ID \n");
-        int userChoice = uc.nextInt();
+        int userChoice = input.nextInt();
     
         //asking user for new value for selected detail
-        Scanner input1 = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
     
         switch(userChoice){
             case 1:
                 System.out.println("Enter new first name:");
-                String newFName = input1.nextLine();
+                String newFName = scan.nextLine();
                 obj.firstName =  newFName;
                 break;
             case 2:
                 System.out.println("Enter new last name:");
-                String newLName = input1.nextLine();
+                String newLName = scan.nextLine();
                 obj.lastName =  newLName;
                 break;
             case 3:
                 System.out.println("Enter new city:");
-                String newCity = input1.nextLine();
+                String newCity = scan.nextLine();
                 obj.cityName =  newCity;
                 break;
             case 4:
                 System.out.println("Enter new state:");
-                String newState = input1.nextLine();
+                String newState = scan.nextLine();
                 obj.state =  newState;
                 break;
             case 5:
                 System.out.println("Enter new zip code:");
-                long newZip = input1.nextLong();
+                long newZip = scan.nextLong();
                 obj.zip =  newZip;
                 break;
             case 6:
                 System.out.println("Enter new phone number:");
-                long newPhNum = input1.nextLong();
+                long newPhNum = scan.nextLong();
                 obj.phoneNum =  newPhNum;
                 break;
             case 7:
                 System.out.println("Enter new email id:");
-                String newEmail = input1.nextLine();
+                String newEmail = scan.nextLine();
                 obj.email = newEmail;
                 break;
             default:
@@ -110,9 +110,8 @@ public class AddressBookMain {
         person2.show(); //prints details
         secondPerson=person2.firstName;
         
-        AddressBookMain.Options(); //selecting options
-        while(d<4){
-            
+        AddressBookMain.options(); //selecting options
+        while(d<4){  
             //switch-case for checking users selected option
             switch(d){
             case 1: //adding another person details
@@ -124,26 +123,31 @@ public class AddressBookMain {
             case 2: //edit person details
                 //asking user which detail to edit
                 
-                Scanner input = new Scanner(System.in);
+                Scanner scan = new Scanner(System.in);
                 System.out.println("Whose details u wanna change? \n1)."+ firstPerson + "\n2)."+ secondPerson);
 
-                int opt1 = input.nextInt();
+                int opt1 = scan.nextInt();
                 if(opt1 == 1){
-                    AddressBookMain.ToChangeDetails(person);
+                    AddressBookMain.toChangeDetails(person);
+                    System.out.println("Succesfully edited " + firstPerson);
                 }if(opt1 == 2){
-                    AddressBookMain.ToChangeDetails(person2);
+                    AddressBookMain.toChangeDetails(person2);
+                    System.out.println("Succesfully edited " + secondPerson);
                 }
+                
                 break;
             
             case 3: // delete person details
-                Scanner inp = new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
                 System.out.println("Whose details u wanna change? \n1)."+ firstPerson + "\n2)."+ secondPerson);
-                int opt = inp.nextInt();
+                int opt = input.nextInt();
                 if(opt == 1){
                     person = null;
+                    System.out.print("successfully deleted " + firstPerson );
                 }
                 if(opt == 2){
                     person2 = null;
+                    System.out.println("successfully deleted "+ secondPerson);
                 }
                  
             break;
@@ -152,17 +156,19 @@ public class AddressBookMain {
                 break;
             }
             
-            AddressBookMain.Options(); //asking for next action
+            AddressBookMain.options(); //asking for next action
         }
     }
+    
 }
+
 //contacts class
 class Contacts{
 
-    static String firstName, lastName, cityName, state, email;
-    static long zip;
-    static long phoneNum;
-    static String a, s1, s2;
+    String firstName, lastName, cityName, state, email;
+    long zip;
+    long phoneNum;
+    String a, s1, s2;
 
     //constructor to assign the values for details
     Contacts(String fn, String ln, String cn, String sn, long pin, long phN, String eid){
@@ -179,16 +185,17 @@ class Contacts{
     static String letterToUpperCase(String a){
         //s1 substring contains first letter of string a
         String s1 = a.substring(0, 1);
-        //s2 substring contains remaining letters of string a except first
+        //s2 substring contains remaining letters of string
         String s2 = a.substring(1, a.length());
     
-        // change the first letter to uppercase
+        // changing the first letter to uppercase
         s1 = s1.toUpperCase();
     
-        // join the two substrings
+        // joining the two substrings
         a = s1 + s2;
         return a;
     }
+
     //method for printing details
     void show() {
 
